efaf473966df7cffd439e94c983034bdc326d538
Ravi Mori <devravimori@gmail.com>
2023-07-03 17:12:38 +0530

    setup gh-pages for static deployment

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/image-search-engine-using-unsplash-api">
     <App />
   </BrowserRouter>
 );
